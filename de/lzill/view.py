0ef2aedd3ce4556c6116f06e58a931d13fcdeb63
LenardZill <login@lenardzill.de>
2015-05-20 09:24:27 +0200

    testing some settings

--- de/lzill/view.py
+++ de/lzill/view.py
@@ -58,4 +58,4 @@ def alarmlist_category(category):
         return redirect('/alarmlist')
         
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
