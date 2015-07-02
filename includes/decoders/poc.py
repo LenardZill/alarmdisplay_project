2708e80f019fd916eec46178bc1fd0aa6a49e936
LenardZill <login@lenardzill.de>
2015-07-02 16:53:21 +0200

    Test

--- includes/decoders/poc.py
+++ includes/decoders/poc.py
@@ -17,37 +17,38 @@ def decode(freq,decoded):
     bitrate = 0
     timestamp = int(time.time())
     
-    if 'POCSAG1200' in decoded:
-        logging.debug(decoded)
-        bitrate = 12000
-        poc_id = decoded[20:27].replace(' ', '').zfill(7)
-        logging.debug(decoded[39])
-        poc_sub = decoded[39].replace('3', '4').replace('2', '3').replace('1', '2').replace('0', '1')
+    if not 'Enabled demodulators:' in decoded:
+        if 'POCSAG1200' in decoded:
+            logging.debug(decoded)
+            bitrate = 12000
+            poc_id = decoded[20:27].replace(' ', '').zfill(7)
+            logging.debug(decoded[39])
+            poc_sub = decoded[39].replace('3', '4').replace('2', '3').replace('1', '2').replace('0', '1')
         
-    if bitrate == 0:
-        logging.warning('POCSAG Bitrate not found')
-        logging.debug(' - (%s)', decoded)
-    else:
-        logging.debug('POCSAG Bitrate: %s', bitrate)
-        
-        if 'Alpha:' in decoded:
-            poc_text = decoded.split('Alpha:    ')[1].strip().rstrip()('<EOT>').strip()
+        if bitrate == 0:
+            logging.warning('POCSAG Bitrate not found')
+            logging.debug(' - (%s)', decoded)
         else:
-            poc_text = ''
+            logging.debug('POCSAG Bitrate: %s', bitrate)
         
-        if re.search('[0-9]{7}', poc_id):
-            if poc_id == globals.poc_id_old and timestamp < globals.poc_time_old + 5:
-                logging.info('POCSAG%s double alarm: %s within %s second(s)', bitrate, globals.poc_id_old, timestamp-globals.poc_time_old)
-                globals.poc_time_old = timestamp
+            if 'Alpha:' in decoded:
+                poc_text = decoded.split('Alpha:    ')[1].strip().rstrip()('<EOT>').strip()
             else:
-                logging.info('POCSAG%s: %s %s %s ', bitrate, poc_id, poc_sub, poc_text)
-                data = {'ric':poc_id, 'function':poc_sub, 'msg': poc_text, 'bitrate':bitrate, 'description':poc_id}
-                data['functionChar'] = data['function'].replace('1', 'a').replace('2', 'b').replace('3', 'c').replace('4', 'd')
+                poc_text = ''
+        
+            if re.search('[0-9]{7}', poc_id):
+                if poc_id == globals.poc_id_old and timestamp < globals.poc_time_old + 5:
+                    logging.info('POCSAG%s double alarm: %s within %s second(s)', bitrate, globals.poc_id_old, timestamp-globals.poc_time_old)
+                    globals.poc_time_old = timestamp
+                else:
+                    logging.info('POCSAG%s: %s %s %s ', bitrate, poc_id, poc_sub, poc_text)
+                    data = {'ric':poc_id, 'function':poc_sub, 'msg': poc_text, 'bitrate':bitrate, 'description':poc_id}
+                    data['functionChar'] = data['function'].replace('1', 'a').replace('2', 'b').replace('3', 'c').replace('4', 'd')
                 
-                from includes import alarmHandler
-                alarmHandler.processAlarm("POC",freq,data)
+                    from includes import alarmHandler
+                    alarmHandler.processAlarm("POC",freq,data)
                 
-                globals.poc_id_old = poc_id
-                globals.poc_time_old = timestamp
-        else:
-            logging.warning('No valid POCSAG%s RIC: %s', bitrate, poc_id)
+                    globals.poc_id_old = poc_id
+                    globals.poc_time_old = timestamp
+            else:
+                logging.warning('No valid POCSAG%s RIC: %s', bitrate, poc_id)
