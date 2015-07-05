c461b67e63e8bd9b18c76701e51a126395a044d6
LenardZill <login@lenardzill.de>
2015-07-05 20:38:24 +0200

    removed sqlite

--- plugins/sqlite/sqlite.py
+++ plugins/sqlite/sqlite.py
@@ -14,27 +14,29 @@ from includes import globals
 
 def run(typ,freq,data):
     try:
-        try:
-            logging.debug('connect to sqlite')
-            connection = sqlite3.connect(globals.database_path)
-            cursor = connection.cursor()
-            
-            cursor.execute('CREATE TABLE IF NOT EXISTS alarmitems(ric TEXT, function TEXT, message TEXT)')
-            connection.commit()
-        except:
-            logging.exception('cannot connect to sqlite')
-        else:
-            try:
-                logging.debug('insert data')
-                cursor.execute('INSERT INTO ' + globals.database_table + ' VALUES("%s","%s","%s")', data['ric'], data['function'], data['msg'])
-                connection.commit()
-            except:
-                logging.exception('cannot insert data')
-    
-        finally:
-            logging.debug('close sqlite')
-            cursor.close()
-            connection.close()
-            
+    #===========================================================================
+    #     try:
+    #         logging.debug('connect to sqlite')
+    #         connection = sqlite3.connect(globals.database_path)
+    #         cursor = connection.cursor()
+    #         
+    #         cursor.execute('CREATE TABLE IF NOT EXISTS alarmitems(ric TEXT, function TEXT, message TEXT)')
+    #         connection.commit()
+    #     except:
+    #         logging.exception('cannot connect to sqlite')
+    #     else:
+    #         try:
+    #             logging.debug('insert data')
+    #             cursor.execute('INSERT INTO ' + globals.database_table + ' VALUES("%s","%s","%s")', data['ric'], data['function'], data['msg'])
+    #             connection.commit()
+    #         except:
+    #             logging.exception('cannot insert data')
+    # 
+    #     finally:
+    #         logging.debug('close sqlite')
+    #         cursor.close()
+    #         connection.close()
+    #===========================================================================
+        pass
     except:
         logging.exception('unknown error')
