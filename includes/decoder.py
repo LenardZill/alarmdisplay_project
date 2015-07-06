c94d02cf0e85539bf945abd514b98fc9abdecf36
LZill <LZill@PC7-LZill.pav.local>
2015-07-06 09:43:33 +0200

    working on process outputs

--- includes/decoder.py
+++ includes/decoder.py
@@ -5,7 +5,8 @@ import logging
 
 def decode(freq,decoded):
     try:
-        from includes.decoders import poc
-        poc.decode(freq,decoded)
+        if "POCSAG" in decoded:
+            from includes.decoders import poc
+            poc.decode(freq,decoded)
     except:
         logging.exception('cannot start decoder')
