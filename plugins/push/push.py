d9cb10bedae4976ad53fe47a21594711c8c9f99e
LenardZill <login@lenardzill.de>
2015-09-13 12:21:33 +0200

    Push Meldungen überarbeitet.

--- plugins/push/push.py
+++ plugins/push/push.py
@@ -44,22 +44,59 @@ def run(typ,freq,data):
             mailtext = mailtext.replace('%DESCR%', data['description'])
             mailtext = mailtext.replace('%TIME%', time.strftime('%H:%M:%S')).replace('%DATE%', time.strftime('%Y-%m-%d'))
             
-            cat = data['msg'].split('/')[0][-1:].strip()
-            logging.debug('category: ' + cat)
-            if cat == 'B' or cat == 'H' or cat == 'S' or cat == 'P' or cat == 'T': 
-                try:
-                    msg = MIMEText(mailtext)
-                    msg['From'] = globals.sender
-                    msg['To'] = globals.reciever
-                    msg['Subject'] = subject
-                    msg['Date'] = formatdate()
-                    msg['Message-Id'] = make_msgid()
+            split = data['msg'].split('/')
+            alarmnumber = split[0][-6:-1].strip()
+            category = split[0][-1:].strip()
+            keyword = split[1].split(')')[0].strip()
+            street = split[3].replace('(', ' ').replace(')', '').strip()
+            street_addition = split[2].split(':')[1].split('(')[0].strip()
+            country = split[2].split(':')[0].strip()
+            caller = split[2].split('(')[-1].replace(')', '').strip()
+            
+            message = ''
+            messagelist = split[4:]
+            for entry in messagelist:
+                message += entry.strip() + ' '
+            message = message.strip()
+            
+            alarmnumber = alarmnumber.decode('utf-8')
+            category = category.decode('utf-8')
+            keyword = keyword.decode('utf-8')
+            street = street.decode('utf-8')
+            street_addition = street_addition.decode('utf-8')
+            country = country.decode('utf-8')
+            caller = caller.decode('utf-8')
+            message = message.decode('utf-8')
+            
+            mailtext = ''
+            mailtext += 'Datum: ' + time.strftime('%d.%m.%Y') + ' ' + time.strftime('%H:%M:%S') + '\n'
+            mailtext += 'Einsatz-Nr: ' + alarmnumber + '\n'
+            mailtext += 'Kategorie: ' + category + '\n'
+            mailtext += 'Stichwort: ' + keyword + '\n'
+            mailtext += 'Nachricht: ' + message + '\n'
+            mailtext += 'Straße: ' + street + ' ' + street_addition + '\n'
+            mailtext += 'Ort: ' + country + '\n'
+            mailtext += 'Anrufer : ' + caller + '\n'
+            
+            #cat = data['msg'].split('/')[0][-1:].strip() 
+            # if cat == 'B' or cat == 'H' or cat == 'S' or cat == 'P' or cat == 'T': 
+            
+            try:
+                msg = MIMEText(mailtext)
+                msg['From'] = globals.sender
+                msg['To'] = globals.reciever
+                msg['Subject'] = subject
+                msg['Date'] = formatdate()
+                msg['Message-Id'] = make_msgid()
+                if category == 'B' or category == 'H' or category == 'S' or category == 'P' or category == 'T':
+                    msg['Priority'] = 'high'
+                else:
                     msg['Priority'] = 'normal'
-                    server.sendmail(globals.sender, globals.reciever.split(), msg.as_string())
-                except:
-                    logging.error('send email failed')
-                    logging.debug('send email failed', exc_info=True)
-                    raise
+                server.sendmail(globals.sender, globals.reciever.split(), msg.as_string())
+            except:
+                logging.error('send email failed')
+                logging.debug('send email failed', exc_info=True)
+                raise
         except:
             logging.error('poc to email failed')
             logging.debug('poc to email failed', exc_info=True)
