46cda348aef82e84fa6a7be6f3c53916d6620f60
LenardZill <login@lenardzill.de>
2015-09-22 07:43:42 +0200

    test www mysql connection

--- www/www.py
+++ www/www.py
@@ -1,32 +1,37 @@
 #!/usr/bin/python
-# -*- coding: UTF-8 -*-
+# -*- coding: cp1252 -*-
 
-'''
-Created on 11.05.2015
+import logging
+import MySQLdb
 
-@author: LZill
-'''
-
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 from datetime import datetime, timedelta
-import sqlite3
+from includes import globals
 
 app = Flask(__name__)
 
-from includes import globals
-
-@app.route('/display')
-def display():
-    conn = sqlite3.connect(globals.database)
-    c = conn.cursor()
-    c.execute('SELECT * FROM'  + globals.table + 'WHERE address = 1685474 ORDER BY id DESC')
-    alarm = c.fetchone()
-    conn.close()
-
-    if alarm[11] < str(datetime.now() - timedelta(hours=2)):
-        alarm = None
-        
-    return render_template('display.html', alarm=alarm)
-        
+def connect_db():
+    try:
+        connection = MySQLdb.connect(host=globals.config.get('MySQL', 'dbserver'),
+                                            user=globals.config.get('MySQL', 'dbuser'),
+                                            passwd=globals.config.get('MySQL', 'dbpassword'),
+                                            db=globals.config.get('MySQL', 'database'))
+        return connection
+    except:
+        logging.error('cannot connect to MySQL')
+        logging.debug('cannot connect to MySQL', exc_info=True)
+        return
+    
+@app.route('/')
+def show_alarm():
+    db = connect_db()
+    cur = db.cursor()
+    for result in cur.excecute('select * from alarmitems where ric=%s order by id desc limit 1',('',)):
+        print("Rows produced by statement '{}':".format(result.statement))
+        print(result.fetchall())
+    else:
+        print("Number of rows affected by statement '{}': {}".format(result.statement, result.rowcount))
+    
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=88, debug=True)
