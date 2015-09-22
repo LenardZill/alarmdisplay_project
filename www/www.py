039ca36396225dd3a568b5a9e185162f76e1394b
LenardZill <login@lenardzill.de>
2015-09-22 08:29:03 +0200

    changed functions

--- www/www.py
+++ www/www.py
@@ -26,7 +26,7 @@ def show_alarm():
     db = connect_db()
     if db is not None:
         cur = db.cursor()
-        cur.execute('SELECT * FROM alarmitems ORDER BY id DESC LIMIT 1')
+        cur.execute('SELECT * FROM alarmitems WHERE ric=%s ORDER BY id DESC LIMIT 1',('1694418'))
         
         for row in cur:
             print(row)
