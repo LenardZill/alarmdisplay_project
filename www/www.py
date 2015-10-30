adf30e31ed31765060a8a78cc94ca52559184ab2
LenardZill <login@lenardzill.de>
2015-10-30 13:12:28 +0100

    changed the display views

--- www/www.py
+++ www/www.py
@@ -2,14 +2,13 @@
 # -*- coding: cp1252 -*-
 
 import logging
-import MySQLdb
+#import MySQLdb
 
 from flask import Flask, render_template
 
 from includes import globals
 
 app = Flask(__name__)
-
 
 def connect_db():
     try:
@@ -24,18 +23,48 @@ def connect_db():
         logging.debug('cannot connect to MySQL', exc_info=True)
         return
 
+def get_alarms():
+    db = connect_db()
+    rows = []
+    if db is not None:
+        cur = db.cursor()
+        cur.excecute('SELECT time, ric, alarmnumber, category, keyword, street, street_addition, country, caller, message FROM alarmitems ORDER BY id DESC LIMIT 10')
+        rows = cur.fetchall()
+        
+    return rows
+
+
+def get_alarm():
+    db = connect_db()
+    row = []
+    if db is not None:
+        cur = db.cursor()
+        cur.excecute('SELECT time, ric, alarmnumber, category, keyword, street, street_addition, country, caller, message FROM alarmitems ORDER BY id DESC LIMIT 1')
+        row = cur.fetchone()
+        
+    return row
+    
 
 @app.route('/')
 def show_alarm():
-    db = connect_db()
-    alarm = []
-    if db is not None:
-        cur = db.cursor()
-        cur.execute('SELECT * FROM alarmitems ORDER BY id WHERE DESC LIMIT 1')
-
-        for row in cur:
-            alarm = row
-    return render_template('display.html', alarm=alarm)
+    
+    #alarm = []
+    alarm = ["29.10.2015 08:47", "11111", "1", "T", "Testeinsatz", "Musterstrasse", "Strassenzusatz", "Grande", "Max Mustermann", "Testalarmmeldung"]
+    alarms = [("29.10.2015 08:47", "11111", "1", "T", "Testeinsatz", "Musterstrasse", "Strassenzusatz", "Grande", "Max Mustermann", "Testalarmmeldung"), 
+              ("29.10.2015 08:47", "11111", "1", "T", "Testeinsatz", "Musterstrasse", "Strassenzusatz", "Grande", "Max Mustermann", "Testalarmmeldung"), 
+              ("29.10.2015 08:47", "11111", "1", "T", "Testeinsatz", "Musterstrasse", "Strassenzusatz", "Grande", "Max Mustermann", "Testalarmmeldung"),
+              ("29.10.2015 08:47", "11111", "1", "T", "Testeinsatz", "Musterstrasse", "Strassenzusatz", "Grande", "Max Mustermann", "Testalarmmeldung"),
+              ("29.10.2015 08:47", "11111", "1", "T", "Testeinsatz", "Musterstrasse", "Strassenzusatz", "Grande", "Max Mustermann", "Testalarmmeldung"),
+              ("29.10.2015 08:47", "11111", "1", "T", "Testeinsatz", "Musterstrasse", "Strassenzusatz", "Grande", "Max Mustermann", "Testalarmmeldung"),
+              ("29.10.2015 08:47", "11111", "1", "T", "Testeinsatz", "Musterstrasse", "Strassenzusatz", "Grande", "Max Mustermann", "Testalarmmeldung"),
+              ("29.10.2015 08:47", "11111", "1", "T", "Testeinsatz", "Musterstrasse", "Strassenzusatz", "Grande", "Max Mustermann", "Testalarmmeldung"),
+              ("29.10.2015 08:47", "11111", "1", "T", "Testeinsatz", "Musterstrasse", "Strassenzusatz", "Grande", "Max Mustermann", "Testalarmmeldung"),
+              ("29.10.2015 08:47", "11111", "1", "T", "Testeinsatz", "Musterstrasse", "Strassenzusatz", "Grande", "Max Mustermann", "Testalarmmeldung")]
+    
+    alarm = get_alarm()
+    alarms = get_alarms()
+    
+    return render_template('display.html', alarm=alarm, alarms=alarms)
 
 
 if __name__ == '__main__':
